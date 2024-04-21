b3be25b5de84b85d1111527cab9947339dcd80d4
karltan0328 <1416299489@qq.com>
2024-04-21 13:55:30 +0000

    Site updated: 2024-04-21 13:55:29

---
+++ anzhiyu/random.js
@@ -0,0 +1,56 @@
+var posts=["/bugs/bugs-while-running-code/","/dissertation-notes/dissertation-summary/","/environment/climb-over-the-great-wall/","/environment/configuration-file-tmux/","/environment/configuration-file-vim/","/environment/cuda-installation-tutorial-in-windows11/","/environment/mysql8.0-installation-tutorial-in-ubuntu20.04/","/environment/online-expansion-of-aliyun-server-cloud-disk/","/environment/python-dl-package-installation-tutorial/","/notes-out-class/common-linux-commands/","/notes-out-class/detailed-explanation-of-mAP-in-object-detection/","/notes-out-class/detailed-explanation-of-rotation-representation-in-pose-estimation/","/dissertation-notes/2017/Vaswani_et_al-2017-Attention_is_all_you_need/","/dissertation-notes/2018/Xiang_et_al-2018-PoseCNN/","/dissertation-notes/2019/Tillet_et_al-2019-Triton/","/dissertation-notes/2019/Wang_et_al-2019-Normalized_Object_Coordinate_Space_for_Category-Level_6D_Object_Pose_and_Size_Estimation/","/dissertation-notes/2021/Dosovitskiy_et_al-2021-An_Image_is_Worth_16x16_Words/","/dissertation-notes/2023/Fan_et_al-2023-POPE/","/dissertation-notes/2023/Nguyen_et_al-2023-NOPE/","/dissertation-notes/2023/Woo_et_al-2023-ConvNeXt_V2/","/notes-out-class/d2l/00/","/notes-out-class/d2l/01/","/notes-out-class/d2l/02/","/notes-out-class/d2l/03/","/notes-out-class/d2l/04/","/notes-out-class/d2l/05/","/notes-out-class/d2l/06/","/notes-out-class/d2l/07/","/notes-out-class/d2l/08/","/notes-out-class/d2l/09/","/notes-out-class/d2l/10/","/notes-out-class/d2l/11/","/notes-out-class/d2l/12/","/notes-out-class/d2l/13/","/notes-out-class/d2l/14/","/notes-out-class/d2l/15/","/notes-out-class/d2l/16/","/notes-out-class/d2l/17/","/notes-out-class/d2l/18/","/notes-out-class/d2l/19/","/notes-out-class/d2l/20/","/notes-out-class/d2l/21/","/notes-out-class/d2l/22/","/notes-out-class/d2l/23/","/notes-out-class/d2l/24/","/notes-out-class/d2l/25/","/notes-out-class/d2l/26/","/notes-out-class/d2l/27/","/notes-out-class/d2l/28/","/notes-out-class/d2l/29/","/notes-out-class/d2l/29_2/","/notes-out-class/d2l/30/","/notes-out-class/d2l/31/","/notes-out-class/d2l/32/","/notes-out-class/d2l/33/","/notes-out-class/d2l/34/","/notes-out-class/d2l/35/","/notes-out-class/d2l/36/","/notes-out-class/d2l/37/","/notes-out-class/d2l/38/","/notes-out-class/d2l/39/","/notes-out-class/d2l/40/","/notes-out-class/d2l/41/","/notes-out-class/d2l/42/","/notes-out-class/d2l/43/","/notes-out-class/d2l/44/","/notes-out-class/d2l/45/","/notes-out-class/d2l/46/","/notes-out-class/d2l/47/","/notes-out-class/d2l/47_2/","/notes-out-class/d2l/48/","/notes-out-class/d2l/49/","/notes-out-class/d2l/50/","/notes-out-class/d2l/51/","/notes-out-class/d2l/52/","/notes-out-class/d2l/53/","/notes-out-class/d2l/54/","/notes-out-class/d2l/55/","/notes-out-class/d2l/56/","/notes-out-class/d2l/57/","/notes-out-class/d2l/58/","/notes-out-class/d2l/59/","/notes-out-class/d2l/60/","/notes-out-class/d2l/61/","/notes-out-class/d2l/62/","/notes-out-class/d2l/63/","/notes-out-class/d2l/64/","/notes-out-class/d2l/65/","/notes-out-class/d2l/66/","/notes-out-class/d2l/67/","/notes-out-class/d2l/68/","/notes-out-class/d2l/69/","/notes-out-class/d2l/70/","/notes-out-class/d2l/71/","/notes-out-class/d2l/72/","/notes-out-class/d2l/73/","/ac-diary/acwing/autumn-daily-question-2023/acwing3724/","/ac-diary/acwing/autumn-daily-question-2023/acwing5017/","/ac-diary/acwing/autumn-daily-question-2023/acwing5081/","/ac-diary/acwing/autumn-daily-question-2023/acwing5164/","/ac-diary/acwing/autumn-daily-question-2023/acwing5165/","/ac-diary/acwing/autumn-daily-question-2023/acwing5166/","/ac-diary/acwing/autumn-daily-question-2023/acwing5170/","/ac-diary/acwing/autumn-daily-question-2023/acwing5179/","/ac-diary/acwing/autumn-daily-question-2023/acwing5180/","/ac-diary/acwing/autumn-daily-question-2023/acwing5181/","/ac-diary/acwing/autumn-daily-question-2023/acwing5183/","/ac-diary/acwing/autumn-daily-question-2023/acwing5198/","/ac-diary/acwing/autumn-daily-question-2023/acwing5199/","/ac-diary/acwing/summer-daily-question-2023/acwing3321/","/ac-diary/acwing/summer-daily-question-2023/acwing3325/","/ac-diary/acwing/summer-daily-question-2023/acwing3326/","/ac-diary/acwing/summer-daily-question-2023/acwing3748/","/ac-diary/acwing/summer-daily-question-2023/acwing3752/","/ac-diary/acwing/summer-daily-question-2023/acwing4114/","/ac-diary/acwing/summer-daily-question-2023/acwing4118/","/ac-diary/acwing/summer-daily-question-2023/acwing4122/","/ac-diary/acwing/summer-daily-question-2023/acwing4382/","/ac-diary/acwing/summer-daily-question-2023/acwing4440/","/ac-diary/acwing/summer-daily-question-2023/acwing4443/","/ac-diary/acwing/summer-daily-question-2023/acwing4633/","/ac-diary/acwing/summer-daily-question-2023/acwing4672/","/ac-diary/acwing/summer-daily-question-2023/acwing4736/","/ac-diary/acwing/summer-daily-question-2023/acwing4737/","/ac-diary/acwing/summer-daily-question-2023/acwing4738/","/ac-diary/acwing/summer-daily-question-2023/acwing4740/","/ac-diary/acwing/summer-daily-question-2023/acwing4741/","/ac-diary/acwing/summer-daily-question-2023/acwing4742/","/ac-diary/acwing/summer-daily-question-2023/acwing4908/","/notes-out-class/taking-the-postgraduate-entrance-examination/calculus/01/"];function toRandomPost(){
+    pjax.loadUrl(posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-cn/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"小林coding","link":"https://xiaolincoding.com/","avatar":"https://xiaolincoding.com/logo.png","descr":"图解计算机网络、操作系统、计算机组成、数据库，让天下没有难懂的八股文！","siteshot":"https://cdn.jsdelivr.net/gh/xiaolincoder/ImageHost4@main/%E7%BD%91%E7%AB%99%E5%B0%81%E9%9D%A2.png","color":"vip","tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-blog@1.1.6/img/post/common/anzhiy.cn.jpg","color":"vip","tag":"技术"},{"name":"Akilarの糖果屋","link":"https://akilar.top/","avatar":"https://npm.elemecdn.com/akilar-friends@latest/avatar/akilar.top.jpg","descr":"期待您的光临！","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/09/02/6311fc39c5966.webp","color":"vip","tag":"技术"},{"name":"张洪Heo","link":"https://blog.zhheo.com/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/02/6311fc38f1465.webp","descr":"分享设计与科技生活","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/09/02/6311fc3959f82.webp","color":"vip","tag":"技术"},{"name":"Tianli","link":"https://tianli-blog.club/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/11/11/636db0d451fd0.webp","descr":"分享设计与科技生活","siteshot":"https://img02.anheyu.com/adminuploads/1/2022/11/07/6368520c9e4e7.webp","color":"vip","tag":"技术"},{"name":"贰猹的小窝","link":"https://noionion.top/","avatar":"https://pub-noionion.oss-cn-hangzhou.aliyuncs.com/head.jpg","descr":"用这生命中的每一秒，给自己一个不后悔的未来","recommend":true},{"name":"Xayah","link":"https://acmezone.top/","avatar":"https://avatars.githubusercontent.com/u/44322170","descr":"这世界上有多少人，就有多少道。莫问前程，各行各道。同道为亲，道不同不相为谋。生如长河，渡船千艘，唯自渡方是真渡。","recommend":true},{"name":"Mufanc","link":"https://blog.mufanc.xyz/","avatar":"https://blog.mufanc.xyz/assets/images/avatar.jpeg","descr":"永不落幕の前奏詩","recommend":false},{"name":"cyp0633's Blog","link":"https://cyp0633.icu/","avatar":"https://cyp0633-public-resource.oss-cn-qingdao.aliyuncs.com/blog/cyp0633-icon.jpg","descr":"我们都曾是海滩上玩沙堡的孩子。现在仍是，只不过不在海滩上罢了。","recommend":false},{"name":"B1ue1nWh1te","link":"https://www.seaeye.cn/","avatar":"https://img.seaeye.cn/img/seaeye/portrait.jpg","descr":"流水不争先，争的是滔滔不绝。","recommend":false},{"name":"我不是咕咕鸽","link":"https://blog.laoda.de/","avatar":"https://gao.ee/gugu/","descr":"服务器折腾不完全指南，定期分享一些好玩的东西，不定期记录生活。","recommend":true},{"name":"浮云翩迁之间","link":"https://blognas.hwb0307.com/","avatar":"https://blognas.hwb0307.com/logo.jpg","descr":"百代繁华一朝都，谁非过客；千秋明月吹角寒，花是主人。","recommend":false},{"name":"鸦鸦的巢穴","link":"https://crowya.com/","avatar":"https://img.crowya.com/wp-content/uploads/2023/05/crowya.jpg","descr":"在乌鸦统治的世界里，洁白的羽毛是有罪的。","recommend":false},{"name":"小冰","link":"https://zfe.space/","avatar":"https://zfe.space/images/headimage.png","descr":"做个有梦想的人！","recommend":false},{"name":"FranzKafka Yu","link":"https://coderfan.net/","avatar":"https://coderfan.net/wp-content/uploads/2021/06/facebook_cover_photo_1.svg","descr":"xui作者。","recommend":true},{"name":"❖星港◎Star☆","link":"https://blog.starsharbor.com/","avatar":"https://bu.dusays.com/2023/09/27/6513053bebdb2.webp","descr":"以博客记录生活与热爱！","recommend":false},{"name":"虚拟号","link":"https://xunihao.net/","avatar":"https://xunihao.net/wp-content/uploads/2020/11/190x120.jpg","descr":"主要介绍如何开通国外手机号。","recommend":true},{"name":"ELuyee","link":"https://www.eluyee.com/","avatar":"https://www.eluyee.com/wp-content/uploads/2021/11/Logo-ELuyee-100x97.png","descr":"主要介绍虚拟信用卡的相关使用方法。","recommend":true}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
